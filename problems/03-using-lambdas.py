4acd64cee5cc4dd0ee519de1ae4fb91cca307f83
takashi <takashi.sh@proton.me>
2024-05-23 19:57:26 -0700

    03 done

--- problems/03-using-lambdas.py
+++ problems/03-using-lambdas.py
@@ -6,8 +6,9 @@
 # returns a lambda that prints `str` `i` times.
 
 # Write your code here.
+
 def string_multi_print(str):
-    pass
+    return lambda i: print(str * i)
 
 string_multi_print('hello ')(2)  # Prints "hello hello "
 string_multi_print('wahoo ')(3)  # Prints "wahoo wahoo wahoo "
